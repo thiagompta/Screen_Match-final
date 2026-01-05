bac81248d9eb4bb4568569d72bcb7e57cacdde86
Thiago Silva <thiagompta008@hotmail.com>
2026-01-05 00:06:28 -0300

    Adiciona projeto Screen Match e configura o gitignore

---
+++ src/Filme.java
@@ -0,0 +1,11 @@
+public class Filme {
+        String nome;
+        int anoDeLancamento;
+        boolean incluidoNoPlano;
+        double avaliacao;
+        int totalDeAvaliacao;
+        int duracaoEmMinutos;
+
+        void  exibeFichaTecnica()
+
+}

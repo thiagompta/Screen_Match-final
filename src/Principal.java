5b58dcecfb7038b5c73153ad61afaeb2bdf2d8ee
Thiago Silva <thiagompta008@hotmail.com>
2026-01-06 19:49:22 -0300

    Refatora classe Filme para package modelos

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,19 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setDuracaoEmMinutos(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5 );
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
-        System.out.println((meuFilme.pegaMedia()));
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
+
+        System.out.println(meuFilme.pegaMedia());
     }
 
 }

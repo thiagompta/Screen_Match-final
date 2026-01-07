02b0aacdfdc71906597a50b16cdd16492d0ef11f
Thiago Silva <thiagompta008@hotmail.com>
2026-01-07 19:33:03 -0300

    Reaproveitando modelos com mesmo nome e comportamentos

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,14 +7,25 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setDuracaoEmMinutos(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do Filme: "+ meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5 );
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
+        //System.out.println(meuFilme.pegaMedia());
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: "+ lost.getDuracaoEmMinutos());
 
-        System.out.println(meuFilme.pegaMedia());
+
+
     }
 
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,60 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-        private String nome;
-        private int anoDeLancamento;
-        private boolean incluidoNoPlano;
-        private double somaDasAvaliacoes;
-        private int totalDeAvaliacao;
-        private int duracaoEmMinutos;
+public class Filme extends Titulo {
+        private String diretor;
 
-        public String getNome() {
-                return nome;
+        public String getDiretor() {
+                return diretor;
         }
 
-        public int getAnoDeLancamento() {
-                return anoDeLancamento;
-        }
-
-        public boolean isIncluidoNoPlano() {
-                return incluidoNoPlano;
-        }
-
-        public int getDuracaoEmMinutos() {
-                return duracaoEmMinutos;
-        }
-
-        public int getTotalDeAvaliacao(){
-            return  totalDeAvaliacao;
-        }
-
-        public void setNome(String nome) {
-                this.nome = nome;
-        }
-
-        public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-                this.incluidoNoPlano = incluidoNoPlano;
-        }
-
-        public void setAnoDeLancamento(int anoDeLancamento) {
-                this.anoDeLancamento = anoDeLancamento;
-        }
-
-        public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-                this.duracaoEmMinutos = duracaoEmMinutos;
-        }
-
-        public void  exibeFichaTecnica(){
-                System.out.println("nome do br.com.alura.screenmatch.modelos.Filme: " + nome);
-                System.out.println("Ano de Lançamento: " + anoDeLancamento);
-                System.out.println("Duração em minutos: " +duracaoEmMinutos);
-                System.out.println("Incluído no plano: " +incluidoNoPlano);
-        }
-        public void avalia(double nota){
-                somaDasAvaliacoes += nota;
-                totalDeAvaliacao++;
-        }
-        public double pegaMedia(){
-                return somaDasAvaliacoes/totalDeAvaliacao;
+        public void setDiretor(String diretor) {
+                this.diretor = diretor;
         }
 }
